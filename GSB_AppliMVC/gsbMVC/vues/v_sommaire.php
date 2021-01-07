b085d629c26b5ea600ece926742c897ef09839ee
Dorian <dorian161100@hotmail.fr>
2021-01-07 14:23:35 +0100

    Update v_sommaire.php

--- GSB_AppliMVC/gsbMVC/vues/v_sommaire.php
+++ GSB_AppliMVC/gsbMVC/vues/v_sommaire.php
@@ -1,30 +1,33 @@
-﻿    <!-- Division pour le sommaire -->
-    <div id="menuGauche">
+﻿<!-- Division pour le sommaire -->
+<div id="menuGauche">
      <div id="infosUtil">
-    
+
         <h2>
-    
+
 </h2>
-    
-      </div>  
+
+      </div>
         <ul id="menuList">
-			<li >
-				  Visiteur :<br>
-				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
-			</li>
+            <li >
+                  Visiteur :<br>
+                <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+            </li>
          <?php if($_SESSION['role'] == "comptable"){
             echo '<li class="smenu">
-              <a href="index.php?uc=validerFicheFrais&action=selectionnerMoisPersonne" title="Valider les fiches de frais">Valider les fiches de frais</a>
+              <a href="index.php?uc=etatFrais&action=selectionnerMoisPersonne" title="Valider les fiches de frais">Valider les fiches de frais</a>
            </li>';
+           echo '<li class="smenu">
+           <a href="index.php?uc=suiviFrais&action=selectionnerMoisPersonne" title="Suivi du paiement des fiches de frais">Suivi du paiement des fiches de frais</a>
+        </li>';
            }else{
            echo '<li class="smenu">
               <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
            </li>';
+           echo '<li class="smenu">
+           <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+               </li>';
            }?>
-           <li class="smenu">
-              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
-           </li>
- 	         <li class="smenu">
+              <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
            </li>
          </ul>
